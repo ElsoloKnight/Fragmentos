7f1d82a0da9feccb196afa1ee6b264a9c2f5f2d6
ElsoloKnight <juaneio@hotmail.com>
2026-05-25 21:15:35 -0600

    easy so easygoing

---
+++ app/src/main/java/com/example/fragmentos/BlankFragment.kt
@@ -0,0 +1,68 @@
+package com.example.fragmentos
+
+import android.content.Context
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.fragmentos.databinding.FragmentBlankBinding
+
+const val TAG = "Fragment"
+
+
+class BlankFragment : Fragment() {
+    var _binding: FragmentBlankBinding? = null
+    val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        Log.d(TAG, "onCreateView")
+        _binding = FragmentBlankBinding.inflate(inflater,
+                container, false)
+        return binding.root
+        return inflater.inflate(R.layout.fragment_blank, container, false)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate")
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        Log.d(TAG, "onDetach")
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        Log.d(TAG, "onAttach")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume")
+    }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        Log.d(TAG, "onDestroyView")
+        _binding = null
+    
+
+    }
+
+}
